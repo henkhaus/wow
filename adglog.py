057200f752d942c770c9c702702a9d1f5f367fd1
asus <rhenkhaus@gmail.com>
2016-12-06 17:54:28 -0600

    fixed loging for adg.py

--- adglog.py
+++ adglog.py
@@ -8,7 +8,7 @@ import pymongo
 from pymongo import MongoClient
 from wowlib import wowapi, queries, log
 import time, os
-
+from time import sleep
 
 logname = os.path.splitext(__file__)[0]
 
@@ -19,8 +19,38 @@ db = client.wow
 data = wowapi.auctionurl('Shadow-Council')
 print("retrieved data")
 posts = db.auctiondata
+import sys
+
+# Print iterations progress
+def printProgress (iteration, total, prefix = '', suffix = '', decimals = 1, barLength = 100):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        barLength   - Optional  : character length of bar (Int)
+    """
+    formatStr = "{0:." + str(decimals) + "f}"
+    percent = formatStr.format(100 * (iteration / float(total)))
+    filledLength = int(round(barLength * iteration / float(total)))
+    bar = '█' * filledLength + '-' * (barLength - filledLength)
+    sys.stdout.write('\r%s |%s| %s%s %s' % (prefix, bar, percent, '%', suffix)),
+    if iteration == total:
+        sys.stdout.write('\n')
+    sys.stdout.flush()
+
+# 
+# Sample Usage
+# 
 
 
+
+# make a list
+items = list(range(0, 100))
+i = 0
 
 @log.log(logname)
 def get_data():
@@ -36,9 +66,20 @@ def get_data():
     auctions =posts.find().distinct('auc')
     for auction in auctions:
         auction_list.append(auction)
+    l = len(data)
     print ("Auction List Created")
     #Iterate through data returned from wowapi
+
+    # Initial call to print 0% progress
+    i=0
+    printProgress(i, l, prefix = 'Progress:', suffix = 'Complete', barLength = 50)
+   
+        # Do stuff...
+        
+        # Update Progress Bar
+       
     for auction  in data:
+        #sleep(0.00001)
         row = data[count]
         #create new json, this allows you to add data not returned from wowapi
         newrow = {'buyout': row['buyout'],
@@ -66,15 +107,18 @@ def get_data():
         if queries.binary_search(auction_list,newrow['auc'])== True:
             posts.update_one({'auc':newrow['auc']},{'$set':{'timeupdated': timestamp}})
             updated +=1
-            print (updated)
+            #print (updated)
         else: 
             posts.insert_one(newrow)
             newcount +=1
             offset = 25-len(newrow['owner'])
-            print ('New Auction Created by: '+newrow['owner'] + ' '*offset+ ' Total Count :'+ str(count) + "      New auction count : "+ str(newcount) )
+            #print ('New Auction Created by: '+newrow['owner'] + ' '*offset+ ' Total Count :'+ str(count) + "      New auction count : "+ str(newcount) )
         
-        count +=1
-
+            count +=1
+        i += 1
+        printProgress(i, l, prefix = 'Progress:', suffix = 'Complete', barLength = 50)
 get_data()
-
+print("Total Auctions : "+ str(len(data)))
+print("updated : "+str(updated))
+print("new auctions : "+str(count))
 
--- oldcode/adg.py
+++ oldcode/adg.py
@@ -1,26 +1,29 @@
 import pymongo
 from pymongo import MongoClient
-import urllib2, json
+import urllib2
+import json
 
 client = MongoClient()
 
 db = client.wow
 collection = db['ah']
-#Variables
+# Variables
 apikey = "8sv23p3ruq39kfng2phrrau3nstevp4j"
 realm = "Shadow-Council"
 
-#Build url for auction data requests
-def buildurl (realm, key):
+# Build url for auction data requests
+
+
+def buildurl(realm, key):
     base = "https://us.api.battle.net/wow/auction/data/"
     mid = "?locale=en_US&apikey="
 
-    print (str(base + realm+mid + key))
+    print(str(base + realm + mid + key))
 
-    return str(base + realm+mid + key)
+    return str(base + realm + mid + key)
 
 
-#grab header file
+# grab header file
 url = buildurl(realm, apikey)
 req = urllib2.Request(url)
 weburl = urllib2.urlopen(req).read()
@@ -29,12 +32,12 @@ encoding = weburl.decode('UTF-8')
 
 data = json.loads(encoding)
 
-#gather data from initial return
+# gather data from initial return
 time = (data['files'][0]['lastModified'])
 url = (data['files'][0]['url'])
 
 
-#get auction data
+# get auction data
 req = urllib2.Request(url)
 weburl = urllib2.urlopen(req).read()
 encoding = weburl.decode('UTF-8')
@@ -45,9 +48,8 @@ posts = db.posts
 posts.insert_many(data['auctions'])
 
 
-
-result =0
+result = 0
 for post in posts:
     result += 1
 
-print (result)
+print(result)
