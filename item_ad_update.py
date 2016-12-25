e6b10c341bef73e82d050b04e885baa74a10118f
thagler1 <tdhagler1@gmail.com>
2016-12-25 10:13:49 -0600

    classify auction items.

--- item_ad_update.py
+++ item_ad_update.py
@@ -1,12 +1,10 @@
-#Purpose: the script takes a unique list from the item collection to update the itemname field in the
-#   auctiondata collection
+# Purpose: the script takes a unique list from the item collection to update the itemname field in the
+# auctiondata collection
 import pymongo
 from pymongo import MongoClient
-from wowlib import wowapi, queries
-import time
+from wowlib import itemclasses as profile
 
-
-#connection informattion
+# connection information
 client = MongoClient("mongodb://76.31.221.129:27017/")
 
 print ('Initiating db connection and getting wow data')
@@ -15,31 +13,28 @@ print("retrieved data")
 posts = db.auctiondata
 itemdb = db.items
 
-
-
-#create list of known users
-knownitems = []
-dbitems = itemdb.distinct('id')
-for item in dbitems:
-    knownitems.append(item)
-print (len(knownitems)) 
-
-
-#aggregates user name and server name return is {_id:{username:"",server:""}}
-
-
-
+# create list of known users
+item_cursor = itemdb.find()
+item_list = []
+for line in item_cursor:
+    item_list.append(line)
+print("list created")
+# aggregates user name and server name return is {_id:{username:"",server:""}}
 
 count = 0
-for item in knownitems:
+for row in item_list:
+    x = row
     try:
-            iname = itemdb.find_one({'id':item})
-            posts.update({'item':item},{'$set':{'itemname':iname['name']}}, multi= True)
-            count +=1
+        itemdata = profile.itemclass(row)
+        print(itemdata)
+        classname = str(itemdata[0])
+        subclass = str(itemdata[1])
+        posts.update({'item': row['id'], 'itemname':"none defined"},{'$set': {'itemname': row['name'],'item_class':classname,'item_subclass': subclass}}, multi=True)
+        count +=1
+        print(count)
 
-            
-
-    except: 
-        print("somethings wrong!!!!")
-
-print (count)
+    except Exception as e:
+        print(e)
+        posts.update({'item': row['id']}, {'$set': {'itemname': row['name']}}, multi=True)
+        count += 1
+        print("failed standard update")
