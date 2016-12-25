23fede3b0287216fe2108197e35ef212e70c713f
thagler1 <tdhagler1@gmail.com>
2016-12-25 16:50:56 -0600

    uses aggregation pipelines to update and classify items

--- testing/item_class_pipeline_query.py
+++ testing/item_class_pipeline_query.py
@@ -1,31 +1,45 @@
+from wowlib import itemclasses as profile
 import pymongo
 from pymongo import MongoClient
-from matplotlib import pyplot as plt
 import datetime
-
+from wowlib import wowapi, queries
 client = MongoClient("mongodb://76.31.221.129:27017/")
 
-db = client.wowtest
-db2 = client.wow
-auctions = db2.auctiondata
-posts = db.items
-#get the most recent returm
-m = auctions.find().distinct('timeupdated')
-maxtimestamp = max(m)
-#print (maxtimestamp)
+db = client.wow
+auctions = db.auctiondata
+items = db.items
 
+#create date pipeline of all auctions with missing item info
+pipeline = auctions.aggregate([{'$match':{'item_class': "none defined"}},
+                              {'$project':{'item':'$item'}}])
 
-#Create list of items from item collection that match metal and ore category (7,7)
-test1 = posts.aggregate([{'$match':{'itemClass':7,'itemSubClass':7}},{'$project':{'itemnum' :'$id'}}])
+# create a distinct set of item numbers from pipeline
+missing_set = set([])
+for obj in pipeline:
+    missing_set.add(obj['item'])
+print(len(missing_set))
 
+#iterate through set
+for i, item in enumerate(missing_set):
+    itemdoc = items.find_one({'id': item})
+    try:
+        #this runs if the item does not exist in item collection. it will add it to the collection
+        if itemdoc == None:
+            itemdoc = wowapi.itemquery(item)
+            items.insert_one(itemdoc)
+    except Exception as e:
+        print(e)
+    if itemdoc != None:
+        #this runs if the item is in the item collection
+        try:
+            itemdata = profile.itemclass(itemdoc)
+            print(itemdata)
+            classname = str(itemdata[0])
+            subclass = str(itemdata[1])
+            auctions.update({'item': item}, {'$set': {'itemname': itemdoc['name'],
+                                                      'item_class': classname,
+                                                      'item_subclass': subclass}}, multi=True)
 
-#iterate through list and out put grouped data (so far this total all items seen)
-for item in test1:
-    rows =auctions.aggregate([{'$match':{"item":item['itemnum'], 'timeupdated':maxtimestamp}},
-        {'$group':{
-        '_id':'$itemname',
-        'available':{'$sum':'$quantity'},
-        'numSellers':{'$addToSet':'$owner'}
-        }}])
-    for row in rows:
-        print(row['_id']+" - " + str(row['available'])+ " - "+str(row['numSellers']))
+        except Exception as e:
+            print(e)
+            print(itemdoc['id'])
